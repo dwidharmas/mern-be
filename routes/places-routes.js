645abf52180a91ff1c28c6d57d51eac023c03cf3
dwidharmas <dwi.dharma97@gmail.com>
2024-06-28 13:37:04 +0800

    day6-ch94

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -2,9 +2,24 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  console.log("GET Request in Places");
-  res.json({ message: "Get is Working" });
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "EMPIRE STATE BUILDING",
+    description: "One of the most famouse sky scrapper in the world",
+    address: "Fifth Avenue (351) 34th Street (west, 20)",
+    locations: {
+      lat: 40.748817,
+      lng: -73.985428,
+    },
+    creator: "u1",
+  },
+];
+
+router.get("/:pid", (req, res, next) => {
+  const placeId = req.params.pid;
+  const placeData = DUMMY_PLACES.find((place) => place.id === placeId);
+  res.json({ place: placeData });
 });
 
 module.exports = router;
