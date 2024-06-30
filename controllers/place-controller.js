ea9a8f8fd382cec130c3a594348ee8fffa5600f3
dwidharmas <dwi.dharma97@gmail.com>
2024-06-30 15:50:47 +0800

    day7-ch106

--- controllers/place-controller.js
+++ controllers/place-controller.js
@@ -1,6 +1,7 @@
 const uuid = require("uuid");
 const { validationResult } = require("express-validator");
 const HttpError = require("../models/http-error");
+const getCoordsForAddress = require("../util/location");
 
 let DUMMY_PLACES = [
   {
@@ -40,13 +41,20 @@ const getPlacesByUserId = (req, res, next) => {
   res.json({ places: placesData });
 };
 
-const createPlace = (req, res, next) => {
+const createPlace = async (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    throw new HttpError("Invalid input, please check your data", 422);
+    return next(new HttpError("Invalid input, please check your data", 422));
   }
-  const { title, description, coordinates, address, creator } = req.body;
+  const { title, description, address, creator } = req.body;
+
+  let coordinates;
+  try {
+    coordinates = await getCoordsForAddress(address);
+  } catch (error) {
+    return next(error);
+  }
   const createdPlace = {
     id: uuid.v4(),
     title,
--- util/location.js
+++ util/location.js
@@ -1,8 +1,25 @@
-import API_KEY from "./api-key";
+const HttpError = require("../models/http-error");
+const API_KEY = require("./api-key");
+const axios = require("axios");
 
 const getCoordsForAddress = async (address) => {
-  return {
-    lat: "",
-    lng: "",
-  };
+  const response = await axios.get(
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
+      address
+    )}&key=${API_KEY}`
+  );
+  const data = response.data;
+  if (!data || data.status === "ZERO_RESULTS") {
+    const error = new HttpError(
+      "Could not find location for specified address",
+      422
+    );
+    throw error;
+  }
+
+  const coordinates = data.results[0].geometry.location;
+
+  return coordinates;
 };
+
+module.exports = getCoordsForAddress;
