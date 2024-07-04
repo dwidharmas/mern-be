91c258987a062294339e296c5cc68d9f8c6095ca
dwidharmas <dwi.dharma97@gmail.com>
2024-07-04 15:28:53 +0800

    day10-ch141

--- controllers/place-controller.js
+++ controllers/place-controller.js
@@ -48,9 +48,11 @@ const getPlaceById = async (req, res, next) => {
 const getPlacesByUserId = async (req, res, next) => {
   const userId = req.params.uid;
 
-  let placesData;
+  // let placesData;
+  let userWithPlaces;
   try {
-    placesData = await Place.find({ creator: userId });
+    // placesData = await Place.find({ creator: userId });
+    userWithPlaces = await User.findById(userId).populate("places");
   } catch (err) {
     const error = new HttpError(
       "Something when wrong, could not find a place for the provided user id please try again",
@@ -59,14 +61,16 @@ const getPlacesByUserId = async (req, res, next) => {
     return next(error);
   }
 
-  if (!placesData || placesData.length === 0) {
+  if (!userWithPlaces || userWithPlaces.places.length === 0) {
     return next(
       new HttpError("Could not find a places for the provided user id", 404)
     );
   }
 
   res.json({
-    places: placesData.map((place) => place.toObject({ getters: true })),
+    places: userWithPlaces.places.map((place) =>
+      place.toObject({ getters: true })
+    ),
   });
 };
 
