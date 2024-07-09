cdfa158dc548fc3669ef0d311b3ff0035cb67754
dwidharmas <dwi.dharma97@gmail.com>
2024-07-09 15:28:58 +0800

    day13-ch167

---
+++ middleware/file-upload.js
@@ -0,0 +1,24 @@
+const multer = require("multer");
+const uuid = require("uuid");
+
+const MIME_TYPE_MAP = {
+  "image/jpg": "jpg",
+  "image/jpeg": "jpeg",
+  "image/png": "png",
+};
+
+const fileUpload = multer({
+  limits: 500000,
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "uploads/images");
+    },
+    filename: (req, file, cb) => {
+      const ext = MIME_TYPE_MAP[file.mimetype];
+      cb(null, uuid.v1() + "." + ext);
+    },
+  }),
+  fileFilter,
+});
+
+module.exports = fileUpload;
